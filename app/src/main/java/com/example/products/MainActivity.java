0ec666ab4b35553030e964431f89729a23334203
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-23 16:41:17 -0600

    Actualizado...Nefta Siuuu7

--- app/src/main/java/com/example/products/MainActivity.java
+++ app/src/main/java/com/example/products/MainActivity.java
@@ -3,13 +3,34 @@ package com.example.products;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtBarcode;
+    private EditText txtDescription;
+    private EditText txtBrand;
+    private EditText txtCost;
+    private EditText txtPrice;
+    private EditText txtStock;
+    private Button btnSave;
+    private ListView lvProducts;
     GroceriesDbHelper dbHelper=new GroceriesDbHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtBarcode=findViewById(R.id.txt_barcode);
+        txtDescription=findViewById(R.id.txt_description);
+        txtBrand=findViewById(R.id.txt_brand);
+        txtCost=findViewById(R.id.txt_cost);
+        txtPrice=findViewById(R.id.txt_price);
+        txtStock=findViewById(R.id.txt_stock);
+        btnSave=findViewById(R.id.btn_save);
+        lvProducts=findViewById(R.id.lv_products);
     }
 }
