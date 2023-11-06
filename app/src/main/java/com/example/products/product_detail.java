085aea486866fdcdc32fa4e9b130b803cfb6b5ed
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-06 16:35:44 -0600

    Actualizado... Siuuuu5

--- app/src/main/java/com/example/products/product_detail.java
+++ app/src/main/java/com/example/products/product_detail.java
@@ -3,15 +3,38 @@ package com.example.products;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class product_detail extends AppCompatActivity {
+    private EditText txtBarcode;
+    private EditText txtDescription;
+    private EditText txtBrand;
+    private EditText txtCost;
+    private EditText txtPrice;
+    private EditText txtStock;
 
+    private Button btnUpdate;
+    private Button btnDelete;
+    private Product product;
+    private ProductDAO productDAO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+        setContentView(R.layout.activity_main);
+        txtBarcode=findViewById(R.id.txt_barcode);
+        txtDescription=findViewById(R.id.txt_description);
+        txtBrand=findViewById(R.id.txt_brand);
+        txtCost=findViewById(R.id.txt_cost);
+        txtPrice=findViewById(R.id.txt_price);
+        txtStock=findViewById(R.id.txt_stock);
+        btnUpdate=findViewById(R.id.btn_update);
+        btnDelete=findViewById(R.id.btn_delete);
         String barcode=getIntent().getExtras().getString("barcode");
-
+        productDAO= new ProductDAO(this);
+        product=productDAO.getProductByBarcode(barcode);
 
     }
 }
